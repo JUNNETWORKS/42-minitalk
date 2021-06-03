e7c0c9fb3718a7455ce1fe96927330b612b272ef
Junichi Tanaka <junichi200123@gmail.com>
2021-06-03 21:05:05 +0900

    server.hに依存しない

--- Makefile
+++ Makefile
@@ -3,23 +3,19 @@ SERVER_NAME = server
 CLIENT_NAME = client
 CC = gcc
 CFLAGS = -Werror -Wall -Wextra -g
-SERVER_HEADERS = server.h
-SERVER_SRCS := server.c server_sighandlers.c
+SERVER_SRCS = server.c
 CLIENT_SRCS = client.c
 CLIENT_OBJS = $(CLIENT_SRCS:.c=.o)
 SERVER_OBJS = $(SERVER_SRCS:.c=.o)
-SERVER_DEPS = $(SERVER_SRCS:.c=.d)
 
 all: $(NAME)
 
 $(NAME): $(SERVER_NAME) $(CLIENT_NAME)
 
 %.o: %.c
-	# -MMD -MP -MF で依存ファイルリスト生成
-	$(CC) $(CFLAGS)  -MMD -MP -MF $(<:.c=.d) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
--include $(SERVER_DEPS)
-$(SERVER_NAME): $(SERVER_OBJS) $(SERVER_HEADERS)
+$(SERVER_NAME): $(SERVER_OBJS)
 	$(CC) $(CFLAGS) -o $(SERVER_NAME) $(SERVER_OBJS)
 
 $(CLIENT_NAME): $(CLIENT_OBJS)
@@ -28,7 +24,6 @@ $(CLIENT_NAME): $(CLIENT_OBJS)
 clean:
 	${RM} ${SERVER_OBJS}
 	${RM} ${CLIENT_OBJS}
-	${RM} ${SERVER_DEPS}
 
 fclean: clean
 	${RM} ${SERVER_NAME}
--- server.c
+++ server.c
@@ -2,22 +2,27 @@
 #include <unistd.h>
 #include <signal.h>
 
-int	g_char;
-int	g_current_bit;
-
 void	restore_data_from_bit(int bit, pid_t client_pid)
 {
-	g_char = (g_char << 1) | bit;
-	g_current_bit++;
-	if (g_current_bit == 8)
+	static int	current_char;
+	static int	current_bit;
+
+	current_char = (current_char << 1) | bit;
+	current_bit++;
+	if (current_bit == 8)
 	{
-		write(STDOUT_FILENO, &g_char, 1);
-		g_char = 0;
-		g_current_bit = 0;
-		kill(client_pid, SIGUSR1);
+		write(STDOUT_FILENO, &current_char, 1);
+		if (current_char == '\0')
+			kill(client_pid, SIGUSR1);
+		current_char = 0;
+		current_bit = 0;
 	}
-	sleep(2);  // alarm()代わり
-	alarm(2);  // 使用可能関数にはいっていない
+	if (sleep(2) > 0 && current_bit)
+	{
+		current_char = 0;
+		current_bit = 0;
+		write(STDERR_FILENO, "TIMEOUT\n", 8);
+	}
 }
 
 void	sigusr_handler(int signum, siginfo_t *info, void *context)
@@ -51,23 +56,13 @@ int	setup_sig_handlers(void)
 	return (0);
 }
 
-// TODO: ALARM()つかっちゃだめ!!
-// TODO: グローバル変数は1つまで (レビュー項目にそう書いてある)
 // TODO: ちゃんとエラーチェックしましょう
-// TODO: *_bonus.c はいらないかなぁ
 // TODO: 1文字ごとにwrite()すると実行速度が遅くてsignalが詰まる可能性がある. 大きい配列を持っていおいて, それに格納していって, ヌル文字もしくは配列の大きさ分文字が来たらwrite()する.
 // TODO: ACKは1文字ずつではなく, 最後にヌル文字が来た場合に送るようにする
-int	main(int argc, char **argv)
+int	main(void)
 {
 	pid_t				pid;
 
-	if (argc != 1)
-	{
-		write(STDERR_FILENO, "argc is invalid\n", 16);  // TODO
-		return (1);
-	}
-	g_char = 0;
-	g_current_bit = 0;
 	setup_sig_handlers();
 	pid = getpid();
 	write(STDOUT_FILENO, "server_pid: ", 12);
